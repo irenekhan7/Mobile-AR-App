acc886bdbc7aeff19f41b6c7510a52b66562bedd
Leslie King <lesliesusanking@yahoo.com>
2014-03-31 23:58:21 -0400

    RCHS 1850s Ball
    Initial checkin of modified Wikitude sample Image Recognition - Single
    Target files

---
+++ RCHS 1850s Ball/assets/samples/1_Image$Recognition_1_Image$On$Target/js/imagerecognition.js
@@ -0,0 +1,42 @@
+var World = {
+	loaded: false,
+
+	init: function initFn() {
+		/* Disable all sensors in "IR-only" Worlds to save performance. If the property is set to true, any geo-related components (such as GeoObjects and ActionRanges) are active. 
+		If the property is set to false, any geo-related components will not be visible on the screen, and triggers will not fire.*/
+		AR.context.services.sensors = false;
+		this.createOverlays();
+	},
+
+	createOverlays: function createOverlaysFn() {
+		// Initialize Tracker
+		// Important: If you replace the tracker file with your own, make sure to change the target name accordingly.
+		// e.g. replace "pageOne" used for creating the AR.Trackeable2DOBject below, with the name of one of your new target images.
+		this.tracker = new AR.Tracker("assets/rchs_hallway.wtc", {
+			onLoaded: this.worldLoaded
+		});
+
+		// Create overlay for page one
+		var imgOne = new AR.ImageResource("assets/butler.png");
+		var overlayOne = new AR.ImageDrawable(imgOne, 1, {
+			offsetX: -0.15,
+			offsetY: 0
+		});
+		var pageOne = new AR.Trackable2DObject(this.tracker, "1_hallway", {
+			drawables: {
+				cam: overlayOne
+			}
+		});
+	},
+	
+	//thumbnail of the target image
+	worldLoaded: function worldLoadedFn() {
+		var cssDivLeft = " style='display: table-cell;vertical-align: middle; text-align: right; width: 50%; padding-right: 15px;'";
+		var cssDivRight = " style='display: table-cell;vertical-align: middle; text-align: left;'";
+		document.getElementById('loadingMessage').innerHTML =
+		"<div" + cssDivLeft + ">Scan Target &#35;1 (hallway):</div>" +
+			"<div" + cssDivRight + "><img src='assets/hallway.png'></img></div>";
+	}
+};
+
+World.init();
